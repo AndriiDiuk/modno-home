6676e899f5929abb6d51c95d37ee025d44f75cdb
skilnet <r.nemyrka@gmail.com>
2026-02-22 11:27:21 +0200

    refactor: simplify component structure and update icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,64 @@
 import "@/assets/style/globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
+const gilroy = localFont({
+  src: [
+    {
+      path: "../public/fonts/Gilroy/Gilroy-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gilroy/Gilroy-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gilroy/Gilroy-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gilroy/Gilroy-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gilroy/Gilroy-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gilroy/Gilroy-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gilroy/Gilroy-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gilroy/Gilroy-Heavy.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Gilroy/Gilroy-Black.woff2",
+      weight: "950",
+      style: "normal",
+    },
+  ],
+  variable: "--font-gilroy",
 });
 
 export const metadata: Metadata = {
   title: {
-    default: "Payload Next Starter",
-    template: "%s | Payload Next Starter",
+    default: "Modno Home",
+    template: "%s | Modno Home",
   },
-  description: "Starter: Next.js + Payload CMS",
+  description: "Modno Home - Luxury Interior Design",
   openGraph: {
     type: "website",
   },
@@ -26,7 +72,7 @@ export default function RootLayout({
   return (
     <html lang='uk' className='h-full' suppressHydrationWarning>
       <body
-        className={`${inter.variable} antialiased flex flex-col min-h-screen`}
+        className={`${gilroy.variable} font-sans antialiased flex flex-col min-h-screen`}
         suppressHydrationWarning
       >
         {children}
